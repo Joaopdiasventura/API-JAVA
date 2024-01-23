a45bc36821c43929ee300be86f8a5e10a70875f9
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2024-01-23 13:45:02 -0300

    feat: Adicionando o CRUD

--- src/main/java/com/example/agenda/Pessoa/PessoaController.java
+++ src/main/java/com/example/agenda/Pessoa/PessoaController.java
@@ -1,21 +1,75 @@
 package com.example.agenda.Pessoa;
 
 import com.example.agenda.Pessoa.dto.CadastrarPessoaDto;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/agenda")
 @CrossOrigin("*")
 public class PessoaController {
-    @GetMapping()
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("foi");
+
+    private final List<CadastrarPessoaDto> pessoasCadastradas = new ArrayList<>();
+
+    public PessoaController (){
+        for (int i = 1; i <= 100; i++){
+            CadastrarPessoaDto pessoaDto = new CadastrarPessoaDto();
+            pessoaDto.setNome("nome" + i);
+            pessoaDto.setTelefone("telefone" + i);
+            pessoaDto.setIdade(i);
+            pessoasCadastradas.add(pessoaDto);
+        }
     }
 
     @PostMapping("/hello")
-    public void Cadastrar(@RequestBody CadastrarPessoaDto cadastrarPessoaDto){
-        System.out.println(cadastrarPessoaDto.getNome());
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<CadastrarPessoaDto> Cadastrar(@RequestBody @Valid CadastrarPessoaDto cadastrarPessoaDto){
+        pessoasCadastradas.add(cadastrarPessoaDto);
+        return ResponseEntity.ok(cadastrarPessoaDto);
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<List<CadastrarPessoaDto>> Ver (){
+        return ResponseEntity.ok(pessoasCadastradas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CadastrarPessoaDto> VerUnico (@PathVariable("id") UUID id){
+        for (CadastrarPessoaDto pessoa : pessoasCadastradas){
+            if (pessoa.getId().equals(id)){
+                return ResponseEntity.ok(pessoa);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CadastrarPessoaDto> Atualizar (@PathVariable("id") UUID id, @RequestBody @Valid CadastrarPessoaDto atualizarPessoaDto) {
+        for (CadastrarPessoaDto pessoa: pessoasCadastradas){
+            if (pessoa.getId().equals(id)){
+                pessoa.atualizar(atualizarPessoaDto);
+                return ResponseEntity.ok(pessoa);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<CadastrarPessoaDto>> Deletar (@PathVariable("id") UUID id){
+        for (CadastrarPessoaDto pessoa : pessoasCadastradas){
+            if (pessoa.getId().equals(id)){
+                pessoasCadastradas.remove(pessoa);
+                return ResponseEntity.ok(pessoasCadastradas);
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/com/example/agenda/Pessoa/dto/CadastrarPessoaDto.java
+++ src/main/java/com/example/agenda/Pessoa/dto/CadastrarPessoaDto.java
@@ -1,31 +1,56 @@
 package com.example.agenda.Pessoa.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.UUID;
+
 public class CadastrarPessoaDto {
+
+    private final UUID id = UUID.randomUUID();
+
+    @NotBlank(message = "Nome não pode ficar em branco")
     private String nome;
+
+    @NotBlank(message = "Telefone não pode ficar em branco")
     private String telefone;
+
+    @Min(value = 0, message = "Idade não pode ser menor que 0")
     private Integer idade;
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
+    public CadastrarPessoaDto() {}
+
+    public UUID getId() {
+        return id;
     }
 
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public Integer getIdade() {
-        return idade;
+    public String getNome() {
+        return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
     public void setIdade(Integer idade) {
         this.idade = idade;
     }
 
-    public String getNome() {
-        return nome;
+    public void atualizar(CadastrarPessoaDto atualizarPessoaDto) {
+        this.nome = atualizarPessoaDto.getNome();
+        this.idade = atualizarPessoaDto.getIdade();
+        this.telefone = atualizarPessoaDto.getTelefone();
     }
 }
